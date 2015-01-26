ec1914e474d9060e9974b24a96f190dd8d5bb401
Florian Löffler <loe.florian@gmail.com>
2015-01-26 17:41:37 +0100

    * less logging * version bump

--- src/groovy/de/rrze/guery/operator/Operator.groovy
+++ src/groovy/de/rrze/guery/operator/Operator.groovy
@@ -30,7 +30,7 @@ class Operator {
 			result = qb.operationManager.apply(type,req,res)
 		}
 		
-		log.info("Operator ${type} ===> ${result}")
+		log.trace("Operator ${type} ===> ${result}")
 		return result
 	}
 	
