5cfc31ed1fefe92dfb3089352dbefceeaaccfe1e
Florian Löffler <loe.florian@gmail.com>
2015-06-09 14:50:06 +0200

    grr

--- grails-app/assets/javascripts/jquery-querybuilder-2.0.1.js
+++ grails-app/assets/javascripts/jquery-querybuilder-2.0.1.js
@@ -1,2 +1,3 @@
-//= require jQuery-QueryBuilder/2.2.0/query-builder.standalone.min
+//= require jQuery-QueryBuilder/2.0.1/jQuery.extendext
+//= require jQuery-QueryBuilder/2.0.1/query-builder.min
 //= require bootstrap/3.3.4/bootstrap.min
