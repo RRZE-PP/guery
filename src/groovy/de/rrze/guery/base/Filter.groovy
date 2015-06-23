292d7c78512ea88fa356fa2e7c9fb20ae101a699
Florian Löffler <loe.florian@gmail.com>
2015-06-23 08:36:44 +0200

    cleanup old code

--- src/groovy/de/rrze/guery/base/Filter.groovy
+++ src/groovy/de/rrze/guery/base/Filter.groovy
@@ -24,8 +24,6 @@ class Filter {
 	Map<String,String>		values = [:]
     String                  plugin
     Map<String,String>      plugin_config = [:]
-    JavascriptCode          onAfterCreateRuleInput
-    JavascriptCode          onAfterSetValue
 
 	
 	def Filter() {}
@@ -33,14 +31,6 @@ class Filter {
 	def add(Operator o) {
 		o.filter = this
 		operators << o
-	}
-	
-	def setOnAfterCreateRuleInput(String s) {
-		onAfterCreateRuleInput = new JavascriptCode(s)
-	}
-
-	def setOnAfterSetValue(String s) {
-		onAfterSetValue = new JavascriptCode(s)
 	}
 
 	def getField() {
@@ -62,13 +52,11 @@ class Filter {
 		putIfNotEmpty(ret,"vertical")
         putIfNotEmpty(ret,"plugin")
         putIfNotEmpty(ret,"plugin_config")
-        putIfNotEmpty(ret,"onAfterCreateRuleInput")
 
 		if (values) {
 			if (values instanceof LinkedHashMap || values instanceof SortedMap) {
 				// ordered
 				ret['values'] = values.collect { [ (it.key) : (it.value) ] }
-//				ret['values'] = values.collect { it.value } // test for backwards compatibility
 			}
 			else {
 				// unordered
@@ -76,15 +64,6 @@ class Filter {
 			}
 		}
 		
-		// FIXME find better plugin mechanism
-        if (this.plugin?.contains("selectize") && !this.onAfterSetValue) {
-            this.onAfterSetValue = "function(\$rule, value) {" +
-                    "var selectize = \$rule.find('.rule-value-container input')[0].selectize;" +
-                    "selectize.setValue(value);" +
-                    "}"
-        }
-		putIfNotEmpty(ret,"onAfterSetValue")
-
 		// handle operators
 		def flatOperators = []
 		this.operators.each {
