7230a54e9842728099fa460e62809f57846aeb5a
Florian Löffler <florian.loeffler@fau.de>
2016-06-17 16:52:10 +0200

    version bump

--- GueryGrailsPlugin.groovy
+++ GueryGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class GueryGrailsPlugin {
     // the plugin version
-    def version = "0.5.4"
+    def version = "0.5.5"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
     // resources that are excluded from plugin packaging
